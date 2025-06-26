eb07dc2ef84e36d82033aa9964d387aca2191df4
chris_code <chris_code@sdsia.laas.fr>
2025-06-27 00:46:17 +0200

    to vercel

--- app/dashboard/responsable/prayers/page.tsx
+++ app/dashboard/responsable/prayers/page.tsx
@@ -3,17 +3,20 @@ import React, { useEffect, useState } from "react"
 import { allPrayers, getMyPrayers } from "@/utils/prayer_requests";
 import PrayerCard from "@/components/base/PrayerCard";
 import { PrayerRequestType } from "@/types/prayer";
+import LoadingOverlay from "@/components/base/LoadingOverlay";
 
 
 
 const PrayerDashboard = () => {
   const [prayers, setPrayers] = useState<PrayerRequestType[]>([]);
-  
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const get = async() => {
       const {success, data} = await allPrayers();
       if(success)
         setPrayers(data);
+
+      setLoading(false);
     }
     get();
   }, [])
@@ -22,6 +25,11 @@ const PrayerDashboard = () => {
 
   return (
     <div className="p-6 w-full mx-auto">
+      {
+        loading
+        &&
+        <LoadingOverlay />
+      }
       <h2 className="text-2xl font-bold mb-4">Prayer Dashboard</h2>
       <div className="grid md:grid-cols-2 gap-4">
         {prayers.map((prayer, index) => <PrayerCard prayer={prayer} key={index}/>) }
